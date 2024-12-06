9c3d743a7d482adb92d0c1e7fbbba67281d838ad
vuduchuy <huyvudc0611@gmail.com>
2024-12-06 16:07:31 +0700

    update new logic test

--- loadtest-spot.js
+++ loadtest-spot.js
@@ -1,63 +1,80 @@
-import { check } from "k6";
-import http from "k6/http";
+import http from 'k6/http';
 import { SharedArray } from 'k6/data';
+import { check } from "k6";
 
-const tokens = new SharedArray('getTokens', function() {
-    return JSON.parse(open('./tokens.json'));
+const ENDPOINT = 'https://dev-gateway.exchange.sotatek.works/api/v1/spot-order';
+
+const MARK_PRICE = '98404.76';
+const TOKENS = new SharedArray('getTokens', function () {
+    const f = JSON.parse(open('./tokens.json'));
+    return f; // f must be an array
 });
 
 export let options = {
     scenarios: {
         constant_request_rate: {// Scenario name
             executor: 'constant-arrival-rate', // Executor type, in this case 'constant-arrival-rate'
-            rate: 4000,       // Number of requests per second
+            rate: 5000,       // Number of requests per second
             timeUnit: '1s', // Rate unit, in this case requests per second
-            duration: '300s', // Total duration of the test, in this case 5 minute
+            duration: '900s', // Total duration of the test, in this case 10 minute
             preAllocatedVUs: 1000, // Number of pre-allocated virtual users
             maxVUs: 4000 // Maximum number of virtual users
         },
     },
 };
 
+export function randomPrice() {
+    const offsetPrice = parseFloat(MARK_PRICE) / 100 * 4;
+    const minPrice = parseFloat(MARK_PRICE) - offsetPrice;
+    const maxPrice = parseFloat(MARK_PRICE) + offsetPrice;
 
+    return (minPrice + Math.random() * (maxPrice - minPrice)).toFixed(2);
+}
 
+export function randomQuantity() {
+    return parseFloat((Math.random() + 0.1).toFixed(3));
+}
+
+export function randomSide() {
+    const sides = ['buy', 'sell'];
+
+    return sides[Math.floor(Math.random() * sides.length)];
+}
+
+export function getRandomToken(tokens) {
+    return tokens[Math.floor(Math.random() * tokens.length)];
+}
 
 export default function () {
-    const token = tokens[Math.floor(Math.random() * tokens.length)];
-    const tradeType = Math.random() > 0.5 ? 'buy' : 'sell'
-    const price = Number((Math.random() * (98000 - 95000) + 95000).toFixed(2))
-    const quantity = Math.random() * (0.2 - 0.001) + 0.001;
-    const total = (price * quantity).toFixed(2);
+    const token = getRandomToken(TOKENS);
+    const url = ENDPOINT + '/order';
+    const bearerToken = `Bearer ${token}`;
+    const price =  randomPrice()
+    const quantity = randomQuantity()
 
-    console.log({
-        coin: "btc",
-        currency: "usdt",
-        quantity: quantity,
-        tradeType: tradeType,
-        type: "limit",
-        total: total,
-        price: price,
-        marketType: "spot",
-    })
+    const headers = {
+        headers: {
+            Authorization: bearerToken,
+            'Content-Type': 'application/json'
+        },
+    };
+    const requestBid = {
+        "coin": "btc",
+        "currency": "usdt",
+        "quantity": quantity,
+        "tradeType": randomSide(),
+        "type": "limit",
+        "total": (price * quantity).toFixed(2),
+        "price": price,
+        "marketType": "spot",
+    }
+
+    console.log(requestBid)
 
     let res = http.post(
         "https://dev-gateway.exchange.sotatek.works/api/v1/spot-order",
-        JSON.stringify({
-            coin: "btc",
-            currency: "usdt",
-            quantity: quantity,
-            tradeType: tradeType,
-            type: "limit",
-            total: total,
-            price: price,
-            marketType: "spot",
-        }),
-        {
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${token}`,
-            }
-        },
+        JSON.stringify(requestBid),
+        headers
     );
 
 
